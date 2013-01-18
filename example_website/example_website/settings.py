1b32fceb282acb1b6fc93e268e8dfdc0e21fcc77
Pankaj Singh <psjinx@gmail.com>
2013-01-18 18:55:08 +0530

    set absolute path to  sqlite db file

--- example_website/example_website/settings.py
+++ example_website/example_website/settings.py
@@ -1,5 +1,7 @@
 # Django settings for example website project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -7,12 +9,13 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'website.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../example_website.db')),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
